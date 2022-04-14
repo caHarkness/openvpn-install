c26f1e9deca4623e50851a88dd32bb7cfa6b1448
Conner Harkness <caharkness@gmail.com>
2022-04-14 14:20:25 -0600

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-cd ..
-rm -rf openvpn-install
-git clone https://github.com/caHarkness/openvpn-install.git
-cd openvpn-install
-echo $(pwd) > .path
+export REPO="https://github.com/caHarkness/openvpn-install.git"
+
+cd ..                   && sleep 1
+rm -rf openvpn-install  && sleep 1
+git clone "$REPO"       && sleep 1
+cd openvpn-install      && sleep 1
+echo $(pwd) > .path     && sleep 1
 chmod +x *
