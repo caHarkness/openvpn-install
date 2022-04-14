4e3670427f1dc2655d2c5e2400cf3ba4dcf8f259
Conner Harkness <caharkness@gmail.com>
2022-04-14 15:47:35 -0600

    Update new-client.sh

--- new-client.sh
+++ new-client.sh
@@ -21,25 +21,25 @@ go_back
 DATA=$(<template/client.txt)
 DATA=${DATA//__REMOTE_ADDRESS__/$PUBLIC_IP}
 
-echo $DATA > client/$1.ovpn
+echo $DATA > clients/$1.ovpn
 
-echo "<ca>" >> client/$1.ovpn
-cat easyrsa/pki/ca.crt >> client/$1.ovpn
-echo "</ca>" >> client/$1.ovpn
+echo "<ca>" >> clients/$1.ovpn
+cat easyrsa/pki/ca.crt >> clients/$1.ovpn
+echo "</ca>" >> clients/$1.ovpn
 
-echo "<cert>" >> client/$1.ovpn
-sed -ne "/BEGIN CERTIFICATE/,$ p" easyrsa/pki/issued/$1.crt >> client/$1.ovpn
-echo "</cert>" >> client/$1.ovpn
+echo "<cert>" >> clients/$1.ovpn
+sed -ne "/BEGIN CERTIFICATE/,$ p" easyrsa/pki/issued/$1.crt >> clients/$1.ovpn
+echo "</cert>" >> clients/$1.ovpn
 
-echo "<key>" >> client/$1.ovpn
-cat easyrsa/pki/private/$1.key >> client/$1.ovpn
-echo "</key>" >> client/$1.ovpn
+echo "<key>" >> clients/$1.ovpn
+cat easyrsa/pki/private/$1.key >> clients/$1.ovpn
+echo "</key>" >> clients/$1.ovpn
 
-echo "<tls-auth>" >> client/$1.ovpn
-sed -ne "/BEGIN OpenVPN Static key/,$ p" /etc/openvpn/ta.key >> client/$1.ovpn
-echo "</tls-auth>" >> client/$1.ovpn
+echo "<tls-auth>" >> clients/$1.ovpn
+sed -ne "/BEGIN OpenVPN Static key/,$ p" /etc/openvpn/ta.key >> clients/$1.ovpn
+echo "</tls-auth>" >> clients/$1.ovpn
 
 # For making this client not redirect traffic
-echo "pull-filter ignore \"redirect-gateway def1 bypass-dhcp\"" >> client/$1.ovpn
+echo "pull-filter ignore \"redirect-gateway def1 bypass-dhcp\"" >> clients/$1.ovpn
 
 go_back
